97481dfd9aa312227b25cd42dc1ed7279de3edd2
Your Name <biswaldeasrit@gmail.com>
2026-02-20 00:01:04 +0530

    full stack python program

---
+++ pythonniis/address.py
@@ -0,0 +1,23 @@
+a=10
+b=20
+c=30
+print("before swapping a=",a,"b=",b,"c=",c)
+t=c	
+c=b
+b=a
+a=t
+print("after swapping a=",a,"b=",b,"c=",c)
+
+print("e" in "hello")
+print("x" in "hello")
+print("x" not in "hello")
+print("10" in [10,20,30])
+	
+a=[10]
+b=[10]
+c=[20]
+print(a is b)
+print(a is c)
+print(a is not c)
+print(a==b)
+
---
+++ pythonniis/mark.py
@@ -0,0 +1,9 @@
+print("enter five mark")
+m1=70
+m2=90
+m3=85
+m4=78
+m5=55
+print(m1,m2,m3,m4,m5)
+print("total=",m1+m2+m3+m4+m5)
+print("avg mark=",(m1+m2+m3+m4+m5)/5)
