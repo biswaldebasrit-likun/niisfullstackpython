f416c53f7f4baaf5132481a039ea27fc0f69420e
Your Name <biswaldeasrit@gmail.com>
2026-02-24 10:29:32 +0530

    test

---
+++ pythonniis/ifelsestatement/ifdemo4.py
@@ -0,0 +1,5 @@
+print("enter a number")
+no=int(input())
+if no!=0:
+	print("enter number is non zero")
+	
---
+++ pythonniis/ifelsestatement/ifdemo7.py
@@ -0,0 +1,8 @@
+print("enter a number")
+no=int(input())
+if no%2==0:
+    print("even number")
+if no%2!=0:
+    print("odd number")
+if no==0:
+    print("enter number is zero")
